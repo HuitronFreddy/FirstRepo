614f6c880dcd202e05f543c147621dcfc1f3306e
HuitronFreddy <freddy.huitron.tfi@gmail.com>
2023-10-06 14:30:50 -0500

    FutureValueCalculator

---
+++ Week1Project/src/main/java/org/example/FutureValueCalculator.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class FutureValueCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //accept initial deposit
+        System.out.println("What is the initial deposit value: ");
+        double initialDeposit = scanner.nextDouble();
+
+        //accept annual interest rate and convert to daily interest rate
+        System.out.println("What is the annual interest rate of your item: ");
+        double annualInterestRate = scanner.nextDouble();
+        double dailyInterestRate = annualInterestRate / 365;
+
+        //accept user input for years
+        System.out.println("How many years would the item appreciate by: ");
+        double yearsInFuture = scanner.nextDouble();
+
+        double futureValue = calculateFutureValue(initialDeposit, dailyInterestRate, yearsInFuture);
+        double totalInterest = futureValue - initialDeposit;
+
+        System.out.printf("The future value of the item is: $%.2f%n", futureValue);
+        System.out.printf("The total interest earned is: $%.2f%n", totalInterest);
+    }
+    public static double calculateFutureValue(double initalDeposit, double dailyInterestRate, double yearsinFuture){
+        if (dailyInterestRate == 0) {
+            return initalDeposit + (initalDeposit * yearsinFuture);
+        }
+        return initalDeposit * Math.pow(1 + (dailyInterestRate / 100), 365 * yearsinFuture);
+    }
+}
--- Week1Project/src/main/java/org/example/Main.java
+++ Week1Project/src/main/java/org/example/Main.java
@@ -24,7 +24,7 @@ public class Main {
         double monthlyLength = loanLength * 12;
 
         scanner.close();
-
+        //math.pow(1 + monthlyInterest, monthlyLength) = a^b a = 1 + monthlyInterest, b= monthlyLength
         double monthlyPayment = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, monthlyLength))
                 / (Math.pow(1 + monthlyInterest, monthlyLength) - 1);
         double totalInterest = (monthlyPayment * monthlyLength) - principal;
